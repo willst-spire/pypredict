550091efe2571cf5960347222c5a6f0a4f7a4ef4
jint <jesse@nanosatisfi.com>
2014-11-20 16:44:07 -0800

    Fixed qth bug, improved documentation

--- predict.py
+++ predict.py
@@ -19,7 +19,7 @@ class Observer():
             raw = open(path.expanduser(qth)).readlines()
             lines = [l.strip() for l in raw]
             assert len(lines) == 4, "qth file '%s' must contain exactly 4 lines (name, lat, long, alt)" % qth
-            qth = lines[1:]+lines[:1] # move name last
+            qth = lines[1:] # remove name
         # Attempt conversion to format required for predict.quick_find
         assert len(qth) == 3, "qth must be (lat, long, alt) as (float, float, int)" 
         self.qth = (float(qth[0]), float(qth[1]), int(qth[2]))
@@ -44,13 +44,17 @@ class Observer():
             # Need to advance time cursor sufficiently far so predict doesn't yield same pass
             crs = t.end + 60     #seconds seems to be sufficient
 
-# Transit is a convenience class representing a pass of a satellite over a groundstation
+# Transit is a convenience class representing a pass of a satellite over a groundstation.
+# It is both an internal class (Observer.passes returns an iterator of transits) and an external
+# class (importers of the library may instantiate).  
 class Transit():
     def __init__(self, tle, qth, start, end):
         self.observer = Observer(tle, qth)
         self.start = start
         self.end = end
-        self.qth = qth
+        # Let Observer.__init__ handle ETL for us.  Re-use!
+        self.qth = self.observer.qth
+        self.tle = self.observer.tle
 
     # return the name of the satellite transiting
     def satellite(self):
@@ -63,9 +67,11 @@ class Transit():
     def duration(self):
         return self.end - self.start
 
-    def slant(self, timestamp):
-        # TODO: Throw exception if out of start, end bounds?
-        return self.observer.observe(timestamp)
+    def at(self, t):
+        if self.start <= t <= self.end:
+            return self.observer.observe(timestamp)
+        else:
+            raise LookupError("time %f outside of transit window [%f, %f]" % (t, self.start, self.end))
 
     def __repr__(self):
         return "Transit%s"%(self.tle, self.qth, self.start, self.end).__repr__()
